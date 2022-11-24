617294f9d0229f1163a7b51f1386b07bc9a38dbe
unknown <renatahassum@gmail.com>
2022-11-24 19:31:27 -0300

    Home - HomeBody - Search CSS created

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <>
+      <h1>Github API</h1>
+    </>
+  );
+}
---
+++ src/routes/Home/index.tsx
@@ -0,0 +1,11 @@
+import Header from "../../components/Header";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <h1>Desafio Github API</h1>
+      <p>DevSuperior - Escola de programação</p>
+    </>
+  );
+}
